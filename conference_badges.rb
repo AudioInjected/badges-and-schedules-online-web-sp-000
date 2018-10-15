b9840a0dff06ae08b49fe29d25ce46e48725749e
Rafael Batista <rafaelb24@yahoo.com>
2018-10-15 16:50:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,8 @@ end
 
 def assign_rooms(speakers)
   array = []
-  speakers.each_with_index do |name, room + 1|
-  array << "Hello, #{name}! You'll be assigned to room #{room}!"
+  speakers.each_with_index do |name, room|
+  array << "Hello, #{name}! You'll be assigned to room " + room + " !"
   end
   return array
 end
