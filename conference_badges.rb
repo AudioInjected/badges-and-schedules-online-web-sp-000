4abfb3331d8fc50b5f2aedba90db68de2fbd98a4
Rafael Batista <rafaelb24@yahoo.com>
2018-10-15 17:03:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,8 +22,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  attendees.each do |names|
-  puts batch_badge_creator(names)
-  puts assign_rooms(names)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
   end
 end
