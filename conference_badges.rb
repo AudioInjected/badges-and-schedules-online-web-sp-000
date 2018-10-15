42575803f3ece7cf6c285ccfaf90d35c8fb8ef57
Rafael Batista <rafaelb24@yahoo.com>
2018-10-15 16:26:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array_names)
+    array_badge_messages = []
+    array_names.each do |names|
+    array_badge_messages << badge_maker(name)
+  end
+  return array_badge_messages
+end
